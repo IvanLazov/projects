0cb2675a00530585a5f3f83d01334fdd1af2591a
IvanLazov <darkpain1989@gmail.com>
2011-09-30 20:37:04 +0300

    GCD, LCM

---
+++ src/com/clouway/introducingjava/gcd/GCD.java
@@ -0,0 +1,28 @@
+package com.clouway.gcd;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: clouway
+ * Date: 9/7/11
+ * Time: 3:05 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class GCD {
+    /**
+        *  Calculates the greatest common divisor of two numbers
+        * @param firstNumber int - first number
+        * @param secondNumber int - second number
+        * @return  Greatest common divisor
+        */
+    public int greatestCommonDivisor(int firstNumber, int secondNumber) {
+        while (firstNumber != secondNumber) {
+            if (firstNumber > secondNumber) {
+                firstNumber = firstNumber - secondNumber;
+            } else {
+                secondNumber = secondNumber - firstNumber;
+            }
+        }
+
+        return firstNumber;
+    }
+}
---
+++ src/com/clouway/introducingjava/gcd/Main.java
@@ -0,0 +1,20 @@
+package com.clouway.gcd;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: clouway
+ * Date: 9/7/11
+ * Time: 3:08 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Main {
+    public static void main(String[] args) {
+        GCD object1 = new GCD();
+        LCM object2 = new LCM();
+
+        int gcdResult = object1.greatestCommonDivisor(14, 21);
+        int lcmResult = object2.leastCommonMultiple(14, 21, gcdResult);
+        System.out.println("GCD is: " + gcdResult);
+        System.out.println("LCM is: " + lcmResult);
+    }
+}
