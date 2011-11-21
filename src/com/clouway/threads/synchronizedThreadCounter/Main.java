48cab08e4dd05098473e33abb396be6071a083a3
IvanLazov <darkpain1989@gmail.com>
2011-11-21 17:11:00 +0200

    Added Main.java for testing the program

---
+++ src/com/clouway/threads/synchronizedThreadCounter/Main.java
@@ -0,0 +1,18 @@
+package com.clouway.threads.synchronizedThreadCounter;
+
+/**
+ * Created by Ivan Lazov (darkpain1989@gmail.com)
+ */
+public class Main {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        final Counter counter = new Counter();
+
+        ThreadCounter threadCounterOne = new ThreadCounter(counter, 5);
+        ThreadCounter threadCounterTwo = new ThreadCounter(counter, 5);
+
+        new Thread(threadCounterOne).start();
+        new Thread(threadCounterTwo).start();
+    }
+}
