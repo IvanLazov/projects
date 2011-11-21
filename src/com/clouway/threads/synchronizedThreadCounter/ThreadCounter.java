b3e9dda0355c6bd47058ade0eecf66f4e0668730
IvanLazov <darkpain1989@gmail.com>
2011-11-21 17:08:29 +0200

    ThreadCounter implements interface Runnable. This interface provides method run() which will be executed by threads

---
+++ src/com/clouway/threads/synchronizedThreadCounter/ThreadCounter.java
@@ -0,0 +1,27 @@
+package com.clouway.threads.synchronizedThreadCounter;
+
+/**
+ * ThreadCounter implements interface Runnable.
+ * This interface provides method run() which will be executed by threads.
+ *
+ * Created by Ivan Lazov (darkpain1989@gmail.com)
+ */
+public class ThreadCounter implements Runnable {
+
+    private int number;
+    private Counter counter;
+
+    public ThreadCounter(Counter counter, int number) {
+        this.counter = counter;
+        this.number = number;
+    }
+
+    public void run() {
+
+        try {
+            counter.count(number);
+        } catch (InterruptedException e) {
+
+        }
+    }
+}
