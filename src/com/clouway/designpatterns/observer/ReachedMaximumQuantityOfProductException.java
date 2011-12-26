efafad22d8c9ee6223b4ad12ee80955fb661b375
IvanLazov <darkpain1989@gmail.com>
2011-12-26 10:39:34 +0200

    Delete implementation of Observer Design Pattern

--- src/com/clouway/designpatterns/observer/ReachedMaximumQuantityOfProductException.java
+++
@@ -1,8 +0,0 @@
-package com.clouway.designpatterns.observer;
-
-/**
- * Created by Ivan Lazov (darkpain1989@gmail.com)
- */
-public class ReachedMaximumQuantityOfProductException extends RuntimeException {
-
-}
