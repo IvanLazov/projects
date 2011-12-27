ffc7d905b6644115378a842c3e2795c33358f1c9
IvanLazov <darkpain1989@gmail.com>
2011-12-27 11:15:04 +0200

    Delete an implementation of Observer Design Pattern

--- src/com/clouway/designpatterns/observer/SoldProductListener.java
+++
@@ -1,9 +0,0 @@
-package com.clouway.designpatterns.observer;
-
-/**
- * Created by Ivan Lazov (darkpain1989@gmail.com)
- */
-public interface SoldProductListener {
-
-    void onSoldProductUpdate(Product product);
-}
