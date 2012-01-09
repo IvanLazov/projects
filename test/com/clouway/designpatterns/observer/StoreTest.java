c5db5cea78b6a0fb0111d9e5351ceccab3ace844
IvanLazov <darkpain1989@gmail.com>
2012-01-09 15:39:00 +0200

    Updated StoreTest.java

--- test/com/clouway/designpatterns/observer/StoreTest.java
+++ test/com/clouway/designpatterns/observer/StoreTest.java
@@ -1,11 +1,7 @@
 package com.clouway.designpatterns.observer;
 
-import org.jmock.Expectations;
-import org.jmock.Mockery;
-import org.jmock.integration.junit4.JMock;
-import org.jmock.integration.junit4.JUnit4Mockery;
+import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -17,47 +13,42 @@ import static org.junit.Assert.assertEquals;
 /**
  * Created by Ivan Lazov (darkpain1989@gmail.com)
  */
-@RunWith(JMock.class)
 public class StoreTest {
 
     private final Map<String, Product> listOfRegisteredProducts = new HashMap<String, Product>();
     private final List<Product> listOfSoldProducts = new ArrayList<Product>();
 
-    private Mockery context = new JUnit4Mockery();
+    private Store store = new Store(listOfRegisteredProducts);
+    private Product product = new Product("Acer");
 
-    private final RegisteredProductListener registeredProductListener = context.mock(RegisteredProductListener.class);
-    private final SoldProductListener soldProductListener = context.mock(SoldProductListener.class);
+    private MockRegisteredProductListener mockRegisteredProductListener = new MockRegisteredProductListener();
+    private MockSoldProductListener mockSoldProductListener = new MockSoldProductListener();
 
+    @Before
+    public void setUp() {
+        
+        store.register(product);
+    }
+    
     @Test
     public void registerProductInStore() {
-
-        Store store = new Store(listOfRegisteredProducts);
-        Product product = new Product("Acer");
-        store.addRegisterProductListener(new RegisteredProducts(listOfRegisteredProducts));
-        store.register(product);
-        assertEquals(product, listOfRegisteredProducts.get("Acer"));
+        
+        Product sony = new Product("Sony");
+        store.register(sony);
+        assertEquals(sony, listOfRegisteredProducts.get("Sony"));
     }
 
     @Test
     public void registeringProductInStoreShouldNotifyListener() {
 
-        final Product product = new Product("Acer");
-        Store store = new Store(listOfRegisteredProducts);
-        store.addRegisterProductListener(registeredProductListener);
-
-        context.checking(new Expectations(){{
-            oneOf(registeredProductListener).onRegisteredProductUpdate(product);
-        }});
-
+        store.addRegisterProductListener(mockRegisteredProductListener);
         store.register(product);
+        assertEquals(product, mockRegisteredProductListener.product);
     }
 
     @Test
     public void registeredProductMustBeInTheRegisteredProductsList() {
 
-        Product product = new Product("Acer");
-        Store store = new Store(listOfRegisteredProducts);
-        store.addRegisterProductListener(new RegisteredProducts(listOfRegisteredProducts));
         store.register(product);
         assertEquals(product, listOfRegisteredProducts.get("Acer"));
     }
@@ -65,43 +56,45 @@ public class StoreTest {
     @Test
     public void sellProductFromStore() {
 
-        Product product = new Product("Acer");
-        Store store = new Store(listOfRegisteredProducts);
-        store.addRegisterProductListener(new RegisteredProducts(listOfRegisteredProducts));
-        store.addSoldProductListener(new SoldProduct(new ArrayList<Product>()));
-
         store.register(product);
-        store.sell("Acer");
-        assertEquals(null, listOfRegisteredProducts.get("Acer"));
+        store.sell(product.getName());
+        assertEquals(null, listOfRegisteredProducts.get(product.getName()));
     }
 
     @Test
     public void sellingProductFromStoreShouldNotifyListener() {
 
-        final Product product = new Product("Acer");
-        Store store = new Store(listOfRegisteredProducts);
-        store.addRegisterProductListener(new RegisteredProducts(listOfRegisteredProducts));
-        store.addSoldProductListener(soldProductListener);
+        store.addSoldProductListener(mockSoldProductListener);
         store.register(product);
-
-        context.checking(new Expectations(){{
-            oneOf(soldProductListener).onSoldProductUpdate(product);
-        }});
-
         store.sell(product.getName());
     }
 
     @Test
     public void soldProductMustBeInTheSoldProductsList() {
 
-        Product product = new Product("Acer");
-        Store store = new Store(listOfRegisteredProducts);
-
-        store.addRegisterProductListener(new RegisteredProducts(listOfRegisteredProducts));
-        store.addSoldProductListener(new SoldProduct(listOfSoldProducts));
-
+        store.addSoldProductListener(mockSoldProductListener);
         store.register(product);
         store.sell(product.getName());
-        assertEquals(product, listOfSoldProducts.get(0));
+        assertEquals(product, mockSoldProductListener.product);
+    }
+
+    private class MockRegisteredProductListener implements RegisteredProductListener {
+
+        private Product product;
+
+        public void onRegisteredProductUpdate(Product product) {
+
+            this.product = product;
+        }
+    }
+
+    private class MockSoldProductListener implements SoldProductListener {
+
+        private Product product;
+
+        public void onSoldProductUpdate(Product product) {
+
+            this.product = product;
+        }
     }
 }
