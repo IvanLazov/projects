bcb4d4c52059ed00fc1c0b15f86866c8f361cd25
IvanLazov <darkpain1989@gmail.com>
2012-01-10 14:41:44 +0200

    Fix tests

--- test/com/clouway/jdbc/trips/TripBaseTest.java
+++ test/com/clouway/jdbc/trips/TripBaseTest.java
@@ -15,50 +15,65 @@ public class TripBaseTest {
     private TripBase tripBase = new TripBaseImpl(databaseHelper);
     private PeopleBase peopleBase = new PeopleBaseImpl(databaseHelper);
 
-    private Person person = new Person("Ivan", "8912271449", 22, "darkpain@abv.bg");
-    private Person person2 = new Person("Adam", "8510105050", 28, "adam@mail.bg");
+    private Person ivan = new Person("Ivan", "8912271449", 22, "darkpain@abv.bg");
+    private Person martin = new Person("Martin", "8510105050", 28, "adam@mail.bg");
 
-    private Trip trip  = new Trip("8912271449", "2012-01-10", "2012-01-20", "Paris");
-    private Trip trip2 = new Trip("8510105050", "2012-02-10", "2012-02-15", "Paris");
-    private Trip trip3 = new Trip("8510105050", "2012-04-08", "2012-04-15", "New York");
+    private Trip first  = new Trip("8912271449", "2012-01-10", "2012-01-20", "Paris");
+    private Trip second = new Trip("8510105050", "2012-02-10", "2012-02-15", "Paris");
+    private Trip third = new Trip("8510105050", "2012-04-08", "2012-04-15", "New York");
 
     @BeforeClass
     public static void connect() throws SQLException {
 
         databaseHelper.connectToDataSource();
     }
-    
-    @Before
-    public void setUp() throws SQLException {
-                
-        peopleBase.save(person);
-        peopleBase.save(person2);
-        tripBase.save(trip);
-        tripBase.save(trip2);
-        tripBase.save(trip3);
-    }
 
+    @Test
+    public void saveTripInDatabase() throws SQLException {
+
+        savePersons(ivan, martin);
+        saveTrips(first, second, third);
+
+        assertEquals(first, databaseHelper.executeQuery("SELECT * FROM trip", new TripRowMapper()).get(0));
+        assertEquals(second, databaseHelper.executeQuery("SELECT * FROM trip", new TripRowMapper()).get(1));
+        assertEquals(third, databaseHelper.executeQuery("SELECT * FROM trip", new TripRowMapper()).get(2));
+    }
+    
     @Test
     public void changeTripArrivalDate() throws SQLException {
 
-        tripBase.update("8912271449", "2012-01-01", "2012-01-20", "Paris");
-        assertEquals("2012-01-01", tripBase.getAllTrips().get(0).getArrivalDate());
+        savePersons(ivan, martin);
+        saveTrips(first, second, third);
+
+        tripBase.update(ivan.getId(), "2012-11-11", "2012-11-20", "Paris");
+
+        assertEquals("2012-11-11", tripBase.getAllTrips().get(0).getArrivalDate());
+        assertEquals("2012-11-20", tripBase.getAllTrips().get(0).getDepartureDate());
+        assertEquals(second, tripBase.getAllTrips().get(1));
+        assertEquals(third, tripBase.getAllTrips().get(2));
     }
     
     @Test
     public void getAllTrips() throws SQLException {
 
-        assertEquals(trip, tripBase.getAllTrips().get(0));
-        assertEquals(trip2, tripBase.getAllTrips().get(1));
+        savePersons(ivan, martin);
+        saveTrips(first, second, third);
+
+        assertEquals(first, databaseHelper.executeQuery("SELECT * FROM trip", new TripRowMapper()).get(0));
+        assertEquals(second, databaseHelper.executeQuery("SELECT * FROM trip", new TripRowMapper()).get(1));
+        assertEquals(third, databaseHelper.executeQuery("SELECT * FROM trip", new TripRowMapper()).get(2));
     }
 
     @Test
-    public void showCitiesOrderedByNumberOfVisitors() throws SQLException {     
-        
+    public void showCitiesOrderedByNumberOfVisitors() throws SQLException {
+
+        savePersons(ivan, martin);
+        saveTrips(first, second, third);
+
         assertEquals(new City("Paris", 2), tripBase.getAllCitiesOrderedByNumberOfVisits().get(0));
         assertEquals(new City("New York", 1), tripBase.getAllCitiesOrderedByNumberOfVisits().get(1));
     }
-
+    
     @After
     public void deleteData() throws SQLException {
 
@@ -70,5 +85,20 @@ public class TripBaseTest {
     public static void disconnect() throws SQLException {
 
         databaseHelper.disconnectFromDataSource();
+    }
+    
+    private void saveTrips(Trip... trips) throws SQLException {
+        
+        for (Trip aTrip : trips) {
+            tripBase.save(aTrip);
+        }
+    }
+    
+    private void savePersons(Person... persons) throws SQLException {
+        
+        for (Person aPerson : persons) {
+            
+            peopleBase.save(aPerson);
+        }
     }
 }
