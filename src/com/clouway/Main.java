2e48126164c7543d5ddee8e7222c1b843f822aa2
IvanLazov <darkpain1989@gmail.com>
2012-02-02 15:27:47 +0200

    Change code style

--- src/com/clouway/Main.java
+++ src/com/clouway/Main.java
@@ -4,15 +4,15 @@ import org.apache.log4j.Logger;
 import org.apache.log4j.BasicConfigurator;
 
 public class Main {
-    
-    static Logger logger = Logger.getLogger(Main.class);
-    
-    public static void main(String[] args) {
 
-        BasicConfigurator.configure();
-        logger.info("Logger");
+  static Logger logger = Logger.getLogger(Main.class);
 
-        MyClass myClass = new MyClass();
-        System.out.println(myClass.multiply(3, 5));
-    }
+  public static void main(String[] args) {
+
+    BasicConfigurator.configure();
+    logger.info("Logger");
+
+    MyClass myClass = new MyClass();
+    System.out.println(myClass.multiply(3, 5));
+  }
 }
