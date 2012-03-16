fa13d75f73f2b3d0600ba4affeb29b9946d9a692
IvanLazov <darkpain1989@gmail.com>
2012-03-16 18:19:41 +0200

    Deleted UserNotRegisteredException

--- jspservlet/src/com/clouway/jspservlet/onlinebanking/exceptions/UserNotRegisteredException.java
+++
@@ -1,7 +0,0 @@
-package com.clouway.jspservlet.onlinebanking.exceptions;
-
-/**
- * Created by Ivan Lazov (darkpain1989@gmail.com)
- */
-public class UserNotRegisteredException extends RuntimeException {
-}
