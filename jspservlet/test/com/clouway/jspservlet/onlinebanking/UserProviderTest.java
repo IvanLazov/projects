e35ee95e84afa4f43b0fbe206bf95cf2f08e6982
IvanLazov <darkpain1989@gmail.com>
2012-03-28 15:23:49 +0300

    UserProviderTest

---
+++ jspservlet/test/com/clouway/jspservlet/onlinebanking/UserProviderTest.java
@@ -0,0 +1,35 @@
+package com.clouway.jspservlet.onlinebanking;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.sql.ResultSet;
+
+import static junit.framework.Assert.assertEquals;
+
+/**
+ * @author Ivan Lazov <darkpain1989@gmail.com>
+ */
+public class UserProviderTest {
+
+  private DatabaseHelper databaseHelper;
+
+  @Before
+  public void setUp() {
+
+    databaseHelper = new DatabaseHelper();
+    databaseHelper.executeQuery("DELETE FROM account");
+    databaseHelper.executeQuery("DELETE FROM user");
+  }
+
+  @Test
+  public void getUser() {
+    
+    databaseHelper.executeQuery("INSERT INTO user(userId,userName,password) VALUES(?,?,?)", 1, "Ivan", "159159");
+
+    User expected = new User(1, "Ivan", "159159");
+    User actual = databaseHelper.executeQuery("SELECT * FROM user WHERE userName=?", new UserProvider(), "Ivan");
+
+    assertEquals(expected, actual);
+  }
+}
