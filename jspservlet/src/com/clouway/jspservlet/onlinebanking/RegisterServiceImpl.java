f56ebf994c2cfc246cb39aafd2e362bf682ac95b
IvanLazov <darkpain1989@gmail.com>
2012-03-28 15:22:02 +0300

    Implementation of the RegisterService

---
+++ jspservlet/src/com/clouway/jspservlet/onlinebanking/RegisterServiceImpl.java
@@ -0,0 +1,24 @@
+package com.clouway.jspservlet.onlinebanking;
+
+/**
+ * @author Ivan Lazov <darkpain1989@gmail.com>
+ */
+public class RegisterServiceImpl implements RegisterService {
+
+  private DatabaseHelper databaseHelper;
+
+  public RegisterServiceImpl(DatabaseHelper databaseHelper) {
+    this.databaseHelper = databaseHelper;
+  }
+
+  public void register(String userName, String password) {
+
+    try {
+      databaseHelper.executeQuery("INSERT INTO user (userName, password) VALUES(?,?)", userName, password);
+      String userId = databaseHelper.executeQueryResult("SELECT userId FROM user WHERE userName=?", userName);
+      databaseHelper.executeQuery("INSERT INTO account (userId, balance) VALUES(?,?)", userId, 0.0);
+    } catch (DuplicateEntryException e) {
+      throw new UserNameAlreadyExistsException();
+    }
+  }
+}
