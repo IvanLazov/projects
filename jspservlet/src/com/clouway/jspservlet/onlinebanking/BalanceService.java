6b1e843e77aebbb8daf18438bd0e81146fd8dac4
IvanLazov <darkpain1989@gmail.com>
2012-04-02 09:38:22 +0300

    BalanceService interface provides methods for updating and getting user's account balance

--- jspservlet/src/com/clouway/jspservlet/onlinebanking/BalanceService.java
+++ jspservlet/src/com/clouway/jspservlet/onlinebanking/BalanceService.java
@@ -1,24 +1,23 @@
 package com.clouway.jspservlet.onlinebanking;
 
 /**
- * BalanceService interface have methods which we use
- * to update and get user's account balance
+ * BalanceService interface provides methods to update and get user's account balance
  *
  * @author Ivan Lazov <darkpain1989@gmail.com>
  */
 public interface BalanceService {
 
   /**
-   * Update the user account balance
+   * Update user account balance
    *
-   * @param amount - the sum we add to the user's balance
+   * @param amount - the amount we want to add
    */
   void updateBalance(double amount);
 
   /**
    * Returns the user account balance
    *
-   * @return - user balance
+   * @return - user account balance
    */
   double getBalance();
 }
