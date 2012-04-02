c9e1bc2c1b9c772a918687e0efd5ff04807a9505
IvanLazov <darkpain1989@gmail.com>
2012-04-02 09:45:24 +0300

    InvalidDepositAmountException is thrown when a user tries to deposit an invalid amount. In this implementation the invalid amount is less than 1 and bigger than 10.000

--- jspservlet/src/com/clouway/jspservlet/onlinebanking/InvalidDepositAmountException.java
+++ jspservlet/src/com/clouway/jspservlet/onlinebanking/InvalidDepositAmountException.java
@@ -1,6 +1,9 @@
 package com.clouway.jspservlet.onlinebanking;
 
 /**
+ * InvalidDepositAmountException is thrown when a user tries
+ * to deposit an amount less than $1 or greater than $10.000
+ *
  * @author Ivan Lazov <darkpain1989@gmail.com>
  */
 public class InvalidDepositAmountException extends RuntimeException {
