867b86481032376984dda00cbcc3b4706604bd0d
IvanLazov <darkpain1989@gmail.com>
2012-04-02 09:57:39 +0300

    WithdrawService provides method for withdraw from user's account

--- jspservlet/src/com/clouway/jspservlet/onlinebanking/WithdrawService.java
+++ jspservlet/src/com/clouway/jspservlet/onlinebanking/WithdrawService.java
@@ -1,9 +1,16 @@
 package com.clouway.jspservlet.onlinebanking;
 
 /**
+ * WithdrawService provides method for withdraw from user's account
+ *
  * @author Ivan Lazov <darkpain1989@gmail.com>
  */
 public interface WithdrawService {
 
+  /**
+   * Withdraw amount from user's account
+   *
+   * @param amount - the amount we will withdraw
+   */
   void withdraw(double amount);
 }
