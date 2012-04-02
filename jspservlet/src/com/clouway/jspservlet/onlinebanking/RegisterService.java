08f5dbca1f2c10817c3329ec79aa3dc32cdd75e2
IvanLazov <darkpain1989@gmail.com>
2012-04-02 09:53:23 +0300

    RegisterService provides method for registering new user with specified username and password

--- jspservlet/src/com/clouway/jspservlet/onlinebanking/RegisterService.java
+++ jspservlet/src/com/clouway/jspservlet/onlinebanking/RegisterService.java
@@ -1,9 +1,18 @@
 package com.clouway.jspservlet.onlinebanking;
 
 /**
+ * RegisterService provides a method for registering new user
+ * with specified username and password
+ *
  * @author Ivan Lazov <darkpain1989@gmail.com>
  */
 public interface RegisterService {
-  
+
+  /**
+   * Register new user with specified username and password
+   *
+   * @param userName - username we will use to register
+   * @param password - password we will use to register
+   */
   void register(String userName, String password);
 }
