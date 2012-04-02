708b79131732fa9bf46aaf9d93e3940bdfdcc27c
IvanLazov <darkpain1989@gmail.com>
2012-04-02 09:46:55 +0300

    InvalidWithdrawAmountException is thrown when user tries to withdraw amount less than 1 and greater than 10,000

--- jspservlet/src/com/clouway/jspservlet/onlinebanking/InvalidWithdrawAmountException.java
+++ jspservlet/src/com/clouway/jspservlet/onlinebanking/InvalidWithdrawAmountException.java
@@ -1,6 +1,9 @@
 package com.clouway.jspservlet.onlinebanking;
 
 /**
+ * InvalidWithdrawAmountException is thrown when the user tries
+ * to withdraw amount less than $1 and greater than $10,000
+ *
  * @author Ivan Lazov <darkpain1989@gmail.com>
  */
 public class InvalidWithdrawAmountException extends RuntimeException {
