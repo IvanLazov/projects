781b90e79cd3c60868d358f056f64cd292337848
IvanLazov <darkpain1989@gmail.com>
2012-04-02 09:46:18 +0300

    InvalidUserNameExpceiton is thrown when user tries to register with invalid username

--- jspservlet/src/com/clouway/jspservlet/onlinebanking/InvalidUserNameException.java
+++ jspservlet/src/com/clouway/jspservlet/onlinebanking/InvalidUserNameException.java
@@ -1,6 +1,8 @@
 package com.clouway.jspservlet.onlinebanking;
 
 /**
+ * InvalidUserNameException is thrown when the user tries to register with invalid username
+ *
  * @author Ivan Lazov <darkpain1989@gmail.com>
  */
 public class InvalidUserNameException extends RuntimeException {
