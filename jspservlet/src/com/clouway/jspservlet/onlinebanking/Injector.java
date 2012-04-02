ec9664adb9b21b01afc5df1991726b6f90cdb51b
IvanLazov <darkpain1989@gmail.com>
2012-04-02 09:43:41 +0300

    Injector class is used to inject objects

--- jspservlet/src/com/clouway/jspservlet/onlinebanking/Injector.java
+++ jspservlet/src/com/clouway/jspservlet/onlinebanking/Injector.java
@@ -1,17 +1,17 @@
 package com.clouway.jspservlet.onlinebanking;
 
 /**
+ * Injector class is used to inject objects when we need them..
+ * This way we reduce the dependencies in our code.
+ *
  * @author Ivan Lazov <darkpain1989@gmail.com>
  */
 public final class Injector {
 
   private static DatabaseHelper databaseHelper;
-  private static OnlineUserManager onlineUserManager;
   private static RegisterService registerService;
-  private static BalanceService balanceService;
-  private static DepositService depositService;
-  private static WithdrawService withdrawService;
-
+  private static OnlineUserManager onlineUserManager;
+  
   public static DatabaseHelper injectDatabaseHelper() {
     if (databaseHelper == null) {
       databaseHelper = new DatabaseHelper();
@@ -30,28 +30,19 @@ public final class Injector {
     if (onlineUserManager == null) {
       onlineUserManager = new OnlineUserManagerImpl(databaseHelper);
     }
+
     return onlineUserManager;
   }
 
   public static BalanceService injectBalanceService(DatabaseHelper databaseHelper, User user) {
-    if (balanceService == null) {
-      balanceService = new BalanceServiceImpl(databaseHelper, user);
-    }
-    return balanceService;
+    return new BalanceServiceImpl(databaseHelper, user);
   }
 
   public static DepositService injectDepositService(BalanceService balanceService) {
-    if (depositService == null) {
-      depositService = new DepositServiceImpl(balanceService);
-    }
-
-    return depositService;
+    return new DepositServiceImpl(balanceService);
   }
 
   public static WithdrawService injectWithdrawService(BalanceService balanceService) {
-    if (withdrawService == null) {
-      withdrawService = new WithdrawServiceImp(balanceService);
-    }
-    return withdrawService;
+    return new WithdrawServiceImp(balanceService);
   }
 }
