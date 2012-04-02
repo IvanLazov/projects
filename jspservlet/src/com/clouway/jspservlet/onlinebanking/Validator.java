cff5fb675de1930b0de5bdde7061067a79db8f12
IvanLazov <darkpain1989@gmail.com>
2012-04-02 09:57:21 +0300

    Validator class is used to validate entered username and password

--- jspservlet/src/com/clouway/jspservlet/onlinebanking/Validator.java
+++ jspservlet/src/com/clouway/jspservlet/onlinebanking/Validator.java
@@ -1,10 +1,18 @@
 package com.clouway.jspservlet.onlinebanking;
 
 /**
+ * Validator class is used to validate entered username and password
+ *
  * @author Ivan Lazov <darkpain1989@gmail.com>
  */
 public class Validator {
-  
+
+  /**
+   * Validate username and password using regex matches
+   *
+   * @param userName - username to validate
+   * @param password - password to validate
+   */
   public void validate(String userName, String password) {
 
     if (!userName.matches("[a-zA-Z]{3,20}")) {
