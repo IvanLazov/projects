ddf187b8a22d120e46291efa7248688dd0ad7420
IvanLazov <darkpain1989@gmail.com>
2012-04-02 09:40:57 +0300

    DepositServiceImpl class implements DepositService. Provides implementation of the deposit method

--- jspservlet/src/com/clouway/jspservlet/onlinebanking/DepositServiceImpl.java
+++ jspservlet/src/com/clouway/jspservlet/onlinebanking/DepositServiceImpl.java
@@ -1,6 +1,10 @@
 package com.clouway.jspservlet.onlinebanking;
 
 /**
+ * DepositServiceImpl class implements DepositService.
+ * Provides implementation of the deposit method, with which
+ * we can deposit amount to the user's account balance
+ *
  * @author Ivan Lazov <darkpain1989@gmail.com>
  */
 public class DepositServiceImpl implements DepositService {
@@ -11,12 +15,17 @@ public class DepositServiceImpl implements DepositService {
     this.balanceService = balanceService;
   }
 
+  /**
+   * Deposit amount to the user's account balance.
+   * Valid amount must be between 1 and 10,000
+   *
+   * @param amount - the amount we want to deposit
+   */
   public void deposit(double amount) {
 
     if (amount <= 0 || amount > 10000) {
       throw new InvalidDepositAmountException();
     }
-    double currentBalance = balanceService.getBalance();
-    balanceService.updateBalance(currentBalance + amount);
+    balanceService.updateBalance(balanceService.getBalance() + amount);
   }
 }
