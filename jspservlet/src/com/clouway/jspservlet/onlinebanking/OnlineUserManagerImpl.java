00692b39809cd5f7a81250522497e36e993dff59
IvanLazov <darkpain1989@gmail.com>
2012-04-02 09:51:28 +0300

    OnlineUserManagerImpl implements OnlineUserManager methods. Set user online/offline and gets the number of online users

--- jspservlet/src/com/clouway/jspservlet/onlinebanking/OnlineUserManagerImpl.java
+++ jspservlet/src/com/clouway/jspservlet/onlinebanking/OnlineUserManagerImpl.java
@@ -1,6 +1,9 @@
 package com.clouway.jspservlet.onlinebanking;
 
 /**
+ * OnlineUserManagerImpl implements OnlineUserManager methods
+ * Set user online/offline, and get the number of online users
+ *
  * @author Ivan Lazov <darkpain1989@gmail.com>
  */
 public class OnlineUserManagerImpl implements OnlineUserManager {
@@ -11,14 +14,30 @@ public class OnlineUserManagerImpl implements OnlineUserManager {
     this.databaseHelper = databaseHelper;
   }
 
+  /**
+   * Set user online, after successful login
+   *
+   * @param sessionId - user's sessionId
+   * @param userName - userName
+   */
   public void setUserOnline(String sessionId, String userName) {
     databaseHelper.executeQuery("INSERT INTO onlineUser(sessionId,userName) VALUES(?,?)", sessionId, userName);
   }
 
+  /**
+   * Set user offline, after successful logout
+   *
+   * @param sessionId - user's sessionId
+   */
   public void setUserOffline(String sessionId) {
     databaseHelper.executeQuery("DELETE FROM onlineUser WHERE sessionId=?", sessionId);
   }
 
+  /**
+   * Get the number of online users
+   *
+   * @return - number of online users
+   */
   public int getNumberOfOnlineUsers() {
     return Integer.parseInt(databaseHelper.executeQueryResult("SELECT COUNT(DISTINCT userName) FROM onlineUser"));
   }
