1fcd9355827495ac53b1410a6f53b4a3baa2e588
IvanLazov <darkpain1989@gmail.com>
2012-04-06 16:21:45 +0300

    Change CatalogServlet logic.

--- jspservlet/src/com/clouway/jspservlet/catalog/CatalogServlet.java
+++ jspservlet/src/com/clouway/jspservlet/catalog/CatalogServlet.java
@@ -5,22 +5,36 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
 
 /**
  * @author Ivan Lazov <darkpain1989@gmail.com>
  */
 public class CatalogServlet extends HttpServlet {
 
+  private CatalogUserService catalogUserService;
+  
   protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-    
-    String[] books = {"Book 1", "Book 2", "Book 3", "Book 4", "Book 5", "Book 6", "Book 7", "Book 8", "Book 9", "Book 10"};
-    request.setAttribute("books", books);
-    
+
     int currentPage = Integer.parseInt((String) request.getAttribute("currentPage"));
-    
+    int range = Integer.parseInt((String) request.getAttribute("range"));
+
     if (currentPage <= 0) {
-      request.setAttribute("currentPage", "1");
+      currentPage = 1;
     }
+
+    catalogUserService = Injector.injectCatalogUserService(Injector.injectCatalogDatabaseService(Injector.injectDatabaseHelper("bookCatalogDB")));
+    int lastPage = catalogUserService.getNumberOfPages(range);
+
+    if (currentPage >= lastPage) {
+      currentPage = lastPage;
+    }
+
+    List books = catalogUserService.getListOfBooks(currentPage, range);
+
+    request.setAttribute("currentPage", currentPage);
+    request.setAttribute("lastPage", lastPage);
+    request.setAttribute("books", books);
 
     request.getRequestDispatcher("/catalog/bookcatalog.jsp").forward(request, response);
   }
