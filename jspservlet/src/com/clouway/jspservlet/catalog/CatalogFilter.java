6b94cb7b344f2067d8843bd446d963e2d8f73420
IvanLazov <darkpain1989@gmail.com>
2012-04-06 14:53:27 +0300

    Created CatalogFilter. Used to set currentPage and range attributes, and then dispatches the request to /catalogServlet.

---
+++ jspservlet/src/com/clouway/jspservlet/catalog/CatalogFilter.java
@@ -0,0 +1,30 @@
+package com.clouway.jspservlet.catalog;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import java.io.IOException;
+
+/**
+ * @author Ivan Lazov <darkpain1989@gmail.com>
+ */
+public class CatalogFilter implements Filter {
+
+  public void init(FilterConfig filterConfig) throws ServletException {
+
+  }
+
+  public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+
+    servletRequest.setAttribute("currentPage", "1");
+    servletRequest.setAttribute("range", "3");
+    servletRequest.getRequestDispatcher("/catalogServlet").forward(servletRequest, servletResponse);
+  }
+
+  public void destroy() {
+
+  }
+}
