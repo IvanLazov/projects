05b3f7fa3da4f3430d7b852ffbd05e18a66fc12e
IvanLazov <darkpain1989@gmail.com>
2012-04-09 11:03:09 +0300

    CatalogDatabaseService interface provides method for returning a Book object with given bookId, returning the number of books currently in the database, returning a list of Books within a certain range

--- jspservlet/src/com/clouway/jspservlet/catalog/CatalogDatabaseService.java
+++ jspservlet/src/com/clouway/jspservlet/catalog/CatalogDatabaseService.java
@@ -3,10 +3,36 @@ package com.clouway.jspservlet.catalog;
 import java.util.List;
 
 /**
+ * CatalogDatabaseService interface provides method for
+ * - returning a Book object with given bookId
+ * - returning the number of books currently in the database
+ * - returning a list of Books within a certain range
+ *
  * @author Ivan Lazov <darkpain1989@gmail.com>
  */
 public interface CatalogDatabaseService {
-  
+
+  /**
+   * Returns a Book object with given bookId
+   *
+   * @param bookId - bookId
+   * @return - Book object
+   */
+  Book getBook(int bookId);
+
+  /**
+   * Returns the number of books currently in the database
+   *
+   * @return - number of books
+   */
   int getNumberOfBooks();
+
+  /**
+   * Returns a list of Books within a certain range
+   *
+   * @param currentPage - page on which the user is currently
+   * @param range - the number of books we want to get
+   * @return - list of Books
+   */
   List getBooks(int currentPage, int range);
 }
