a5c88229bb701441f6045ce64f79a3192ddecb44
IvanLazov <darkpain1989@gmail.com>
2012-04-09 11:38:23 +0300

    DisplayComments class extends SimpleTagSupport. This way the class is able to display every book from the List of books when it encounters the "displayComments" tag in a specified .jsp page.

---
+++ jspservlet/src/com/clouway/jspservlet/catalog/DisplayComments.java
@@ -0,0 +1,35 @@
+package com.clouway.jspservlet.catalog;
+
+import javax.servlet.jsp.JspException;
+import javax.servlet.jsp.tagext.SimpleTagSupport;
+import java.io.IOException;
+import java.util.List;
+
+/**
+ * DisplayComments class extends SimpleTagSupport.
+ * This way the class is able to display every book from the List of books
+ * when it encounters the "displayComments" tag in a specified .jsp page
+ *
+ * @author Ivan Lazov <darkpain1989@gmail.com>
+ */
+public class DisplayComments extends SimpleTagSupport {
+  
+  private List comments;
+  private String comment;
+  
+  public void setComments(List comments) {
+    this.comments = comments;
+  }
+  
+  public void setComment(String comment) {
+    this.comment = comment;
+  }
+  
+  public void doTag() throws JspException, IOException {
+    
+    for (int i = 0; i < comments.size(); i++) {
+      getJspContext().setAttribute(comment, comments.get(i));
+      getJspBody().invoke(null);
+    }
+  }
+}
