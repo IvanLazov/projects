c593e6e75bb293622efcb83eaa67b5aed20304b4
IvanLazov <darkpain1989@gmail.com>
2012-04-09 11:43:21 +0300

    CommentsDatabaseService provides methods for - saving user comment, and returning list of all comments for specified book by given bookId.

--- jspservlet/src/com/clouway/jspservlet/catalog/CommentsDatabaseService.java
+++ jspservlet/src/com/clouway/jspservlet/catalog/CommentsDatabaseService.java
@@ -3,10 +3,28 @@ package com.clouway.jspservlet.catalog;
 import java.util.List;
 
 /**
+ * CommentsDatabaseService provides methods for
+ * - saving user comment
+ * - returning list of all comments for specified book by given bookId
+ *
  * @author Ivan Lazov <darkpain1989@gmail.com>
  */
 public interface CommentsDatabaseService {
-  
+
+  /**
+   * Save user comment for specified book by given bookId
+   *
+   * @param bookId - bookId
+   * @param userName - userName
+   * @param comment - entered comment by the user
+   */
   void saveComment(int bookId, String userName, String comment);
+
+  /**
+   * Returns a List of Comment objects for specified book by given bookId
+   *
+   * @param bookId - bookId
+   * @return - List of Comment objects
+   */
   List getComments(int bookId);
 }
