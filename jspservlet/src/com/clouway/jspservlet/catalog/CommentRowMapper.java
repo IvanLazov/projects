271881aa28c4172c5292d1cf54cc8fe647c04b95
IvanLazov <darkpain1989@gmail.com>
2012-04-09 11:40:20 +0300

    CommentRowMapper creates a Comment object from given ResultSet.

--- jspservlet/src/com/clouway/jspservlet/catalog/CommentRowMapper.java
+++ jspservlet/src/com/clouway/jspservlet/catalog/CommentRowMapper.java
@@ -4,10 +4,19 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 /**
+ * CommentRowMapper creates a Comment object from given ResultSet
+ *
  * @author Ivan Lazov <darkpain1989@gmail.com>
  */
 public class CommentRowMapper implements RowMapper<Comment> {
 
+  /**
+   * Returns Comment object from given ResultSet
+   *
+   * @param resultSet - the resultSet from which we take the necessary parameters to create the object
+   * @return - Comment object
+   * @throws SQLException - if an error occurs while getting the parameters from the ResultSet
+   */
   public Comment map(ResultSet resultSet) throws SQLException {
     return new Comment(
             Integer.parseInt(resultSet.getString("bookId")),
