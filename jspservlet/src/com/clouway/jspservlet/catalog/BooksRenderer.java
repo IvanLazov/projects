fb2f9b16f2bca5e387ca47ca0df647eb37bb3011
IvanLazov <darkpain1989@gmail.com>
2012-04-10 10:46:30 +0300

    DisplayBooks is renamed to BooksRenderer

--- jspservlet/src/com/clouway/jspservlet/catalog/BooksRenderer.java
+++ jspservlet/src/com/clouway/jspservlet/catalog/BooksRenderer.java
@@ -6,13 +6,13 @@ import java.io.IOException;
 import java.util.List;
 
 /**
- * DisplayBooks class extends SimpleTagSupport.
+ * BooksRenderer class extends SimpleTagSupport.
  * This way the class is able to display every book from the List of books
  * when it encounters the "displayBooks" tag in a specified .jsp page
  *
  * @author Ivan Lazov <darkpain1989@gmail.com>
  */
-public class DisplayBooks extends SimpleTagSupport {
+public class BooksRenderer extends SimpleTagSupport {
 
   private List books;
   private String book;
@@ -27,19 +27,19 @@ public class DisplayBooks extends SimpleTagSupport {
   public void setBook(String book) {
     this.book = book;
   }
-  
+
   public void setCurrentPage(String currentPage) {
     this.currentPage = Integer.parseInt(currentPage);
   }
-  
+
   public void setRange(String range) {
     this.range = Integer.parseInt(range);
   }
-  
+
   public void setLastPage(String lastPage) {
     this.lastPage = Integer.parseInt(lastPage);
   }
-  
+
   public void doTag() throws JspException, IOException {
 
     for (Object book : books) {
