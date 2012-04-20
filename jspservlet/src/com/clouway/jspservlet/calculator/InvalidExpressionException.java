ce540ca548db82f24dea2fa82bfbe1586bc61b34
IvanLazov <darkpain1989@gmail.com>
2012-04-20 17:03:43 +0300

    I deleted what I Could

--- jspservlet/src/com/clouway/jspservlet/calculator/InvalidExpressionException.java
+++
@@ -1,7 +0,0 @@
-package com.clouway.jspservlet.calculator;
-
-/**
- * @author Ivan Lazov <darkpain1989@gmail.com>
- */
-public class InvalidExpressionException extends RuntimeException {
-}
--- jspservlet/src/com/clouway/jspservlet/calculator/Operation.java
+++
@@ -1,9 +0,0 @@
-package com.clouway.jspservlet.calculator;
-
-/**
- * Created by Ivan Lazov (darkpain1989@gmail.com)
- */
-public interface Operation {
-
-  int calculate(int firstNumber, int secondNumber);
-}
